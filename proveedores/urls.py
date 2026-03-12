21061d0b11d501e4e39517f691ddfbc3e9e4cb3f
david pedraza <davlpedraza@soy.sena.edu.co>
2026-03-12 11:04:17 -0500

    agregar nuevo proveedor desde compra

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -5,7 +5,7 @@ app_name = 'proveedores'
 
 urlpatterns = [
     path('listar/', views.listar_proveedores, name='listar'),
-    path('agregar/', views.agregar_proveedor, name='agregar'),
+    path('agregar/', views.agregar_proveedor, name='agregar_proveedor'),
     path('editar/<int:pk>/', views.editar_proveedor, name='editar'),
     path('eliminar/<int:pk>/', views.eliminar_proveedor, name='eliminar'),
     path('detalle/<int:pk>/', views.detalle_proveedor, name='detalle'),
